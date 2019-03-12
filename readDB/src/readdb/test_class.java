229f877fc26ae24f677306c5e441539f3890f49b
bryle99 <brylebaran@gmail.com>
2019-03-12 23:12:37 +0800

    c files

---
+++ readDB/src/readdb/test_class.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package readdb;
+
+/**
+ *
+ * @author wabzs
+ */
+public class test_class {
+    public int x;
+    
+    public int getThis(){
+        x = 10;
+        return x;
+    }
+    
+}
